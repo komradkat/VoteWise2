a0d55737df2271700ad2ac099be4ad9ba5fe9d2e
Sean Lloyd Harold Raquel <seanlloydraquel@gmail.com>
2025-11-29 09:56:34 +0800

    chore: Remove outdated documentation files, configure static root, and adjust header max-width.

--- project_config/settings/base.py
+++ project_config/settings/base.py
@@ -102,6 +102,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For collectstatic
 
 # Media files
 MEDIA_URL = '/media/'
